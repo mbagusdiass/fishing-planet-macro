b03496d0b8e49eab95ca1f2ddd427e544614498c
mbagusdiass <m.bagus.dias.s@gmail.com>
2023-12-22 16:16:48 +0700

    auto fish keep

---
+++ build.py
@@ -0,0 +1,90 @@
+import os
+import random
+import shutil
+import sys
+
+import PyInstaller.__main__
+
+
+def build(name, console, onefile, uac_admin, icon, files, folders):
+	work_path = "build"
+	while os.path.isdir(work_path):
+		work_path = f"build_{random.randint(1, 1_000_000_000)}"
+	work_path = os.path.join(os.path.abspath("."), work_path)
+
+	result_path = os.path.abspath(".")
+
+	if os.path.isfile(os.path.join(result_path, f"{name}.exe")):
+		os.remove(os.path.join(result_path, f"{name}.exe"))
+
+	run_list = ['main.py',
+	            '--noconfirm',
+	            '--clean',
+	            '--name', name,
+	            '--workpath', work_path,
+	            '--specpath', work_path,
+	            '--distpath', result_path]
+
+	if console:
+		run_list.append("--console")
+	else:
+		run_list.append("--noconsole")
+
+	if onefile:
+		run_list.append("--onefile")
+	else:
+		run_list.append("--onedir")
+
+	if uac_admin:
+		run_list.append("--uac-admin")
+
+	if icon != "":
+		icon_path = os.path.join(os.path.abspath("."), icon)
+		if not os.path.isfile(icon_path):
+			raise Exception("Invalid icon!")
+		else:
+			run_list.extend(('--icon', icon_path))
+
+	for file in files:
+		if os.path.isfile(os.path.join(os.path.abspath("."), file)):
+			run_list.extend(('--add-data', f'{os.path.join(os.path.abspath("."), file)};{os.path.dirname(file)}'))
+		else:
+			raise Exception("Invalid file!")
+
+	for folder in folders:
+		if os.path.isdir(folder):
+			for walk in os.walk(folder, followlinks=False):
+				for file in walk[2]:
+					if os.path.isfile(os.path.join(walk[0], file)):
+						run_list.extend(('--add-data', f'{os.path.join(os.path.abspath("."), os.path.join(walk[0], file))};{os.path.dirname(os.path.join(walk[0], file))}'))
+					else:
+						raise Exception("Invalid folder!")
+		else:
+			raise Exception("Invalid folder!")
+
+	PyInstaller.__main__.run(run_list)
+	shutil.rmtree(path=work_path, ignore_errors=True)
+
+def main():
+	name = "Autofish"
+	version = "0.1.5"
+
+	console = False
+	onefile = True
+	uac_admin = False
+	icon = "data\\fish_icon.ico"
+
+	files = [icon]
+	folders = ["run_data"]
+
+	if len(sys.argv) > 1 and sys.argv[1] == "--version":
+		print(version)
+	elif len(sys.argv) > 1 and sys.argv[1] == "--name":
+		print(name)
+	else:
+		name = f"{name}-v{version}"
+		build(name, console, onefile, uac_admin, icon, files, folders)
+
+
+if __name__ == '__main__':
+	main()
